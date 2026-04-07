4fe5a98585c6199bb39c3a5e4c019ffd1f169df6
Julius Santiago <bulius@gmail.com>
2026-04-06 20:11:08 -0700

    Initial Commit

---
+++ tv-clock/tv_clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  tv_clockApp.swift
+//  tv-clock
+//
+//  Created by Julius Santiago on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct tv_clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tv-clockTests/tv_clockTests.swift
@@ -0,0 +1,18 @@
+//
+//  tv_clockTests.swift
+//  tv-clockTests
+//
+//  Created by Julius Santiago on 4/6/26.
+//
+
+import Testing
+
+struct tv_clockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
